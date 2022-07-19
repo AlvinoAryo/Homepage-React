8b1dc6ed20c7bd3015b6fe964e5d920ba98a42f1
Vino aryo <vinoaryo2000@gmail.com>
2022-07-20 00:34:09 +0700

    Adding footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./Pages/Homepage";
 import NavBar from "./Pages/NavBar";
+import Footer from "./Pages/Footer";
 
 function App() {
   return (
     <>
       <div className="min-h-screen text-gray-50">
         <div>
-          <NavBar title="VinoWeb"/>
+          <NavBar title="VinoWeb" />
         </div>
         <div className="font-bold m-2 p-4">
           <HomePage />
+        </div>
+        <div className="sticky top-[100vh]">
+          <Footer />
         </div>
       </div>
     </>
