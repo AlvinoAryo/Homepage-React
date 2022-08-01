c5856db1d9fdaa355f2ec413d125d6e34943b5d6
Vino aryo <vinoaryo2000@gmail.com>
2022-08-01 22:59:05 +0700

    Removing aos in NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,22 +1,18 @@
 import { Link } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init();
 
 const NavBar = (data) => {
   const avatar = setInterval(() => {
     Math.floor(1000 + Math.random() * 9000);
   }, 1000);
   return (
-    <div className="navbar" data-aos="fade-down">
+    <div className="navbar">
       <div className="navbar-start">
         <a href="/" className="btn btn-ghost normal-case text-xl">
           AlvinoAryo
         </a>
       </div>
       <div className="navbar-end">
-        <div className="dropdown dropdown-end transition duration-300 ease-in-out" style={{indexZ: 1,}}>
+        <div className="dropdown dropdown-end transition duration-300 ease-in-out">
           <button className="btn btn-square btn-ghost" tabindex="0">
             <img
               src={`https://avatars.dicebear.com/api/adventurer/${avatar}.svg`}
